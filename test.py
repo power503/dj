cfcc0d406183c10a74f3f7ffae38f233dcd60b0e
power503 <76802443+power503@users.noreply.github.com>
2026-04-03 13:17:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import os
 
-secret_value = os.environ.get("pushplus_TOKEN")
-if secret_value:
-    # 正常工作
-    print("Secret received (not printed): ",secret_value)
-    with open('gitee_stock_result.log', 'w', encoding='utf-8') as f:
-        f.write(secret_value)
+secret_value = os.getenv("pushplus_TOKEN")
+print("Secret received (not printed): ",secret_value)
+with open('gitee_stock_result.log', 'w', encoding='utf-8') as f:
+    f.write(secret_value)
