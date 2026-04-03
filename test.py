f6c88bc863743bb00a0523c90af62950b078fd0f
power503 <76802443+power503@users.noreply.github.com>
2026-04-03 13:05:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 
-secret_value = os.getenv("pushplus_TOKEN")
+secret_value = os.environ.get("pushplus_TOKEN")
 if secret_value:
     # 正常工作
     print("Secret received (not printed): ",secret_value)
